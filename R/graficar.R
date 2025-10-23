d4abfe1d5ce59f357486e2ad1473504b7ac1fa8a
Pedro <pedroc1951@outlook.com>
2025-10-23 09:36:51 -0600

    cambiar media por valor brindado de Y

--- R/graficar.R
+++ R/graficar.R
@@ -345,7 +345,7 @@ Graficar <- R6::R6Class(
     graficar_barras_v = function(x, y = "media"){
       self$grafica <- ggplot2::ggplot(self$tbl, ggplot2::aes(x= !!rlang::sym(x), y = !!rlang::sym(y)))+
         ggchicklet::geom_chicklet(ggplot2::aes(fill = color),width = 0.8 ) +
-        ggplot2::geom_text( ggplot2::aes(label = scales::percent(media, accuracy = 1)),
+        ggplot2::geom_text( ggplot2::aes(label = scales::percent(!!rlang::sym(y), accuracy = 1)),
       size = 5, vjust = -.1, family = self$tema$text$family) +
         ggplot2::labs(caption = self$tbl$pregunta[1])+
         ggplot2::scale_y_continuous(labels = scales::percent_format(accuracy = 1), limits = c(0,1))+
