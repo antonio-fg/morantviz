895d99c046976644fd98909190cbf581a1fd4b29
Rafael Lopez <rafa0823.rl@gmail.com>
2025-12-09 13:07:14 -0600

    Cambios menores en scripts

--- inst/ej_graficar_lineas.R
+++ inst/ej_graficar_lineas.R
@@ -34,9 +34,8 @@
 #' - `self$tbl$pregunta[1]` contiene el texto de la pregunta para
 #'   usarlo como `caption` de la gráfica.
 
-
-### Librerias 
-devtools::load_all(path = "../morantviz/") 
+### Librerias
+devtools::load_all(path = "../morantviz/")
 library(rlang)
 library(dplyr)
 library(purrr)
@@ -44,16 +43,16 @@ library(ggrepel)
 
 
 ################################### Graficar líneas para diferentes llaves ###################################
-# 
+#
 # Para este caso cada llave representa un línea de color diferente. Para definir el color correspondiente, en la
-# paleta de colores se puede especificar el color deseado con la llave o con el nombre 
+# paleta de colores se puede especificar el color deseado con la llave o con el nombre
 
-### Colores 
+### Colores
 colores <- tibble::tribble(
-  ~respuesta,      ~color,
-  "Álvaro Obregón", "#0c4c8a",
-  "Venustiano Carranza", "#95a5a6",
-  "Iztapalapa", "#1b8a0cff"
+  ~respuesta            , ~color      ,
+  "Álvaro Obregón"    , "#0c4c8a"   ,
+  "Venustiano Carranza" , "#95a5a6"   ,
+  "Iztapalapa"          , "#1b8a0cff"
 )
 
 ## Función para generar respuestas aleatorias basada en una lista de categorías
@@ -65,8 +64,9 @@ problemas <- function(cat, llave, n) {
   )
 }
 
-## Lista de categorias 
-categorias <- c( "Baches en calles y avenidas",
+## Lista de categorias
+categorias <- c(
+  "Baches en calles y avenidas",
   "Delincuencia",
   "Falta de agua",
   "Falta de alumbrado",
@@ -76,51 +76,59 @@ categorias <- c( "Baches en calles y avenidas",
   "Otro",
   "Parques y jardines descuidados",
   "Tráfico vehicular",
-  "Transporte público deficiente")
+  "Transporte público deficiente"
+)
 
 ## Base de datos
-bd_problemas_ao <- problemas(cat = categorias,llave = "problemas_ao",n = 100)
-bd_problemas_vc <- problemas(categorias, llave = "problemas_vc",n=100)
-bd_problemas_iz <- problemas(categorias, llave = "problemas_iz", n=100)
+bd_problemas_ao <- problemas(cat = categorias, llave = "problemas_ao", n = 100)
+bd_problemas_vc <- problemas(categorias, llave = "problemas_vc", n = 100)
+bd_problemas_iz <- problemas(categorias, llave = "problemas_iz", n = 100)
 
 
 ## Join de las bases de datos
 bd_respuestas <- list(
   bd_problemas_ao,
   bd_problemas_vc,
-  bd_problemas_iz) |> 
+  bd_problemas_iz
+) |>
   reduce(full_join, by = "id")
 
 
 ### Diccionario de problemas
-dicc <- tibble::tribble(~codigo, ~nombre, ~pregunta,
-                        "problemas_ao","Álvaro Obregón", "¿Cúal considera que es el problema principal de la alcaldía?",
-                        "problemas_vc","Venustiano Carranza","¿Cuál considera que es el problema principal de la alcaldía?",
-                        "problemas_iz","Iztapalapa", "¿Cuál considera que es el problema principal de la alcaldía?",
-                      )
+dicc <- tibble::tribble(
+  ~codigo        , ~nombre               , ~pregunta                                                         ,
+  "problemas_ao" , "Álvaro Obregón"    , "¿Cúal considera que es el problema principal de la alcaldía?" ,
+  "problemas_vc" , "Venustiano Carranza" , "¿Cuál considera que es el problema principal de la alcaldía?" ,
+  "problemas_iz" , "Iztapalapa"          , "¿Cuál considera que es el problema principal de la alcaldía?" ,
+)
 
-### Inicialización del objeto g 
-g <- Graficar$new(bd = bd_respuestas,
-                        diccionario = dicc,
-                        colores = colores,
-                        color_principal = "#340e63",
-                        tema = tema_morant())
+### Inicialización del objeto g
+g <- Graficar$new(
+  bd = bd_respuestas,
+  diccionario = dicc,
+  colores = colores,
+  color_principal = "#340e63",
+  tema = tema_morant()
+)
 
-g$
-  contar_variables(variables = 
-    c("problemas_ao",
-    "problemas_vc",
-    "problemas_iz"))$
-  pegar_diccionario()$
-  calcular_pct()$
-  pegar_colorPorGrupo("nombre")$
-  filtrar_respuesta(valor = c("Baches en calles y avenidas", "Falta de agua", "Falta de recolección de basura"))$
-  graficar_lineas(x ="respuesta", 
-                  freq ="pct", 
-                  grupo = "nombre", 
-                  grupos_seleccion = c("Álvaro Obregón","Venustiano Carranza","Iztapalapa"), 
-                  letra_tam = 8, 
-                  rango_offset = 0.03)
+g$contar_variables(
+  variables = c("problemas_ao", "problemas_vc", "problemas_iz")
+)$pegar_diccionario()$calcular_pct()$pegar_colorPorGrupo(
+  "nombre"
+)$filtrar_respuesta(
+  valor = c(
+    "Baches en calles y avenidas",
+    "Falta de agua",
+    "Falta de recolección de basura"
+  )
+)$graficar_lineas(
+  x = "respuesta",
+  freq = "pct",
+  grupo = "nombre",
+  grupos_seleccion = c("Álvaro Obregón", "Venustiano Carranza", "Iztapalapa"),
+  letra_tam = 8,
+  rango_offset = 0.03
+)
 
 
 ################################### Graficar líneas para una misma llave y diferemtes respuestas ###################################
@@ -135,80 +143,110 @@ problemas <- function(cat, llave, n) {
   )
 }
 ## Respuestas
-categorias <- c("Aprueba mucho",
+categorias <- c(
+  "Aprueba mucho",
   "Aprueba poco",
   "Desaprueba poco",
-  "Desaprueba mucho")
+  "Desaprueba mucho"
+)
 
 ## Generación de las respuestas para el caso de aprobacióm de Rosario entre el 2024 y 2026
 
-bd_aprob_rosario_2024  <- problemas(cat = categorias,llave = "aprob_rosario",n = 100) |> mutate(año = 2024)
-bd_aprob_rosario_2025  <- problemas(cat = categorias,llave = "aprob_rosario",n = 100) |> mutate(año = 2025)
-bd_aprob_rosario_2026  <- problemas(cat = categorias,llave = "aprob_rosario",n = 100) |> mutate(año = 2026)
+bd_aprob_rosario_2024 <- problemas(
+  cat = categorias,
+  llave = "aprob_rosario",
+  n = 100
+) |>
+  mutate(año = 2024)
+bd_aprob_rosario_2025 <- problemas(
+  cat = categorias,
+  llave = "aprob_rosario",
+  n = 100
+) |>
+  mutate(año = 2025)
+bd_aprob_rosario_2026 <- problemas(
+  cat = categorias,
+  llave = "aprob_rosario",
+  n = 100
+) |>
+  mutate(año = 2026)
 
 
-## binding de las respuestas por año 
-bd_total_rosario <- bind_rows(bd_aprob_rosario_2024, bd_aprob_rosario_2025, bd_aprob_rosario_2026) |>
-  mutate(id = row_number()) |> 
-  relocate(id, .before = everything())  |> 
-  select(-matches("año\\.")) %>% 
+## binding de las respuestas por año
+bd_total_rosario <- bind_rows(
+  bd_aprob_rosario_2024,
+  bd_aprob_rosario_2025,
+  bd_aprob_rosario_2026
+) |>
+  mutate(id = row_number()) |>
+  relocate(id, .before = everything()) |>
+  select(-matches("año\\.")) %>%
   mutate(año = as.character(año))
 
 
-### Diccionario 
-dicc <- tibble::tribble(~codigo, ~pregunta,
-"aprob_rosario", "¿Qué le parece la gorbernatura de Rosario?")
+### Diccionario
+dicc <- tibble::tribble(
+  ~codigo         , ~pregunta                                      ,
+  "aprob_rosario" , "¿Qué le parece la gorbernatura de Rosario?"
+)
 
 ## Colores de las respuestas
 colores <- tibble::tribble(
-  ~respuesta,      ~color,
-  "Aprueba mucho",   "#1b8a0cff",
-  "Aprueba poco",   "#0c8a7fff",
-  "Desaprueba poco", "#67b4d7ff",
-  "Desaprueba mucho","#ae2e27ff")
+  ~respuesta         , ~color      ,
+  "Aprueba mucho"    , "#1b8a0cff" ,
+  "Aprueba poco"     , "#0c8a7fff" ,
+  "Desaprueba poco"  , "#67b4d7ff" ,
+  "Desaprueba mucho" , "#ae2e27ff"
+)
 
 
 ### Inicialización de la g
-g <- Graficar$new(bd = bd_total_rosario,
-                        diccionario = dicc,
-                        colores = colores,
-                        color_principal = "#340e63",
-                        tema = tema_morant())
+g <- Graficar$new(
+  bd = bd_total_rosario,
+  diccionario = dicc,
+  colores = colores,
+  color_principal = "#340e63",
+  tema = tema_morant()
+)
 
 ## Utilización de contar variables por grupos para separarlas por año
-g$contar_variables_porGrupos(variables = c("aprob_rosario"),grupos = c("año"))$
-  pegar_diccionario()$
-  calcular_pct()$
-  pegar_color()$
-  graficar_lineas(x ="año", 
-                  freq ="pct", 
-                  grupo = "respuesta", 
-                  letra_tam = 7, 
-                  rango_offset = 0.037)
+g$contar_variables_porGrupos(
+  variables = c("aprob_rosario"),
+  grupos = c("año")
+)$pegar_diccionario()$calcular_pct()$pegar_color()$graficar_lineas(
+  x = "año",
+  freq = "pct",
+  grupo = "respuesta",
+  letra_tam = 7,
+  rango_offset = 0.037
+)
 
 
 ### Otras formas de graficar usando facet_wrap(~ "especificar columna")
-g$contar_variables_porGrupos(variables = c("aprob_rosario"),grupos = c("año"))$
-  pegar_diccionario()$
-  calcular_pct()$
-  pegar_color()$
-  graficar_lineas(x ="año", 
-                  freq ="pct", 
-                  grupo = "respuesta", 
-                  letra_tam = 7, 
-                  rango_offset = 0.037) + 
-  
-g$contar_variables_porGrupos(variables = c("aprob_rosario"),grupos = c("año"))$
-pegar_diccionario()$
-calcular_pct()$
-pegar_color()$
-graficar_lineas(x ="año", 
-                  freq ="pct", 
-                  grupo = "respuesta", 
-                  letra_tam = 7, 
-                  rango_offset = 0.00) + facet_wrap(~respuesta)
+g$contar_variables_porGrupos(
+  variables = c("aprob_rosario"),
+  grupos = c("año")
+)$pegar_diccionario()$calcular_pct()$pegar_color()$graficar_lineas(
+  x = "año",
+  freq = "pct",
+  grupo = "respuesta",
+  letra_tam = 7,
+  rango_offset = 0.037
+)
 
-## Agregar esto para quitar el eje Y cuando se quiera graficar dos gráficas 
+g$contar_variables_porGrupos(
+  variables = c("aprob_rosario"),
+  grupos = c("año")
+)$pegar_diccionario()$calcular_pct()$pegar_color()$graficar_lineas(
+  x = "año",
+  freq = "pct",
+  grupo = "respuesta",
+  letra_tam = 7,
+  rango_offset = 0.00
+) +
+  facet_wrap(~respuesta)
+
+## Agregar esto para quitar el eje Y cuando se quiera graficar dos gráficas
 
 #theme(
 #    axis.title.y  = element_blank(),
--- inst/eje_lineas_clasificacion.R
+++ inst/eje_lineas_clasificacion.R
@@ -1,6 +1,6 @@
 ################################### graficar lineas clasificación ###################################
 
-usethis::use_readme_rmd( open = FALSE )
+usethis::use_readme_rmd(open = FALSE)
 library(encuestar) #si se necesita para el diseño muestral
 library(survey)
 #library(morantviz)
@@ -10,25 +10,35 @@ library(patchwork)
 
 
 ## Dicc del ejemplo
-dicc <- tibble::tribble(~codigo, ~nombre, ~pregunta,
-                        "conoce_pm_astiazaran", "Astiazarán", "Conoce o ha escuchado de (...)",
-                        "conoce_pm_delrio", "Del Río", "Conoce o ha escuchado de (...)",
-                        "conoce_pm_lia", "Lía Limón", "Conoce o ha escuchado de (...)",
-                        "conoce_pm_javier", "Javier López Casarín", "Conoce o ha escuchado de (...)",
-                        "opinion_pm_astiazaran", "Astiazarán","¿Cuál es su opinión sobre (...)?",
-                        "opinion_pm_delrio", "Del Río","¿Cuál es su opinión sobre (...)?"
+dicc <- tibble::tribble(
+  ~codigo                 , ~nombre                  , ~pregunta                             ,
+  "conoce_pm_astiazaran"  , "Astiazarán"            , "Conoce o ha escuchado de (...)"      ,
+  "conoce_pm_delrio"      , "Del Río"               , "Conoce o ha escuchado de (...)"      ,
+  "conoce_pm_lia"         , "Lía Limón"            , "Conoce o ha escuchado de (...)"      ,
+  "conoce_pm_javier"      , "Javier López Casarín" , "Conoce o ha escuchado de (...)"      ,
+  "opinion_pm_astiazaran" , "Astiazarán"            , "¿Cuál es su opinión sobre (...)?" ,
+  "opinion_pm_delrio"     , "Del Río"               , "¿Cuál es su opinión sobre (...)?"
 )
 
 
+g <- Encuesta$new(
+  diseno = encuesta_demo$muestra$diseno,
+  diccionario = dicc,
+  colores = NULL,
+  color_principal = "pink",
+  tema = tema_morant()
+)
 
-g <- Encuesta$new(diseno = encuesta_demo$muestra$diseno,
-                diccionario = dicc,
-                colores = NULL,
-                color_principal = "pink",
-                tema = tema_morant())
-
-g$contar_variables(variables = c("conoce_pm_astiazaran", "conoce_pm_delrio", "conoce_pm_lia", "conoce_pm_javier"), confint = F)
-g$graficar_lineas_clasificacion(titulo='Líneas de clasificación')
+g$contar_variables(
+  variables = c(
+    "conoce_pm_astiazaran",
+    "conoce_pm_delrio",
+    "conoce_pm_lia",
+    "conoce_pm_javier"
+  ),
+  confint = F
+)
+g$graficar_lineas_clasificacion(titulo = 'Líneas de clasificación')
 #g$graficar_lineas_clasificacion(x_var='respuesta',y_var='media')
 
 g$tbl
